1b1899a36f4b27835cc6ae70a82f496de94751bf
Khubaib Jamal <khubaib.j18@gmail.com>
2024-09-09 23:55:30 +0500

    register singleton class and dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:architecture/user_list_cubit.dart';
 import 'package:architecture/user_list_page.dart';
-
+import 'package:architecture/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
+final getIt = GetIt.instance;
 void main() {
+  getIt.registerSingleton<UserRepository>(UserRepository());
   runApp(
     MultiBlocProvider(providers: [
-      BlocProvider(create: (context) => UserListCubit()..fetchUser()),
+      BlocProvider(create: (context) => UserListCubit(getIt())..fetchUser()),
     ], child: const MyApp()),
   );
 }
